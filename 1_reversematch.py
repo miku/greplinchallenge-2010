4b4fa089a3bfd525c4d11881231febc419894e2c
miku <martin.czygan@gmail.com>
2011-01-02 15:20:53 +0100

    added problem descriptions

--- 1_reversematch.py
+++ 1_reversematch.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Level 1
+#   
+#   ----------------------------------------
+# 
+#   Embedded in this block of text is the password for level 2.
+#   The password is the longest substring that is the same in reverse.
+#   
+#   As an example, if the input was "I like racecars that go fast"
+#   the password would be "racecar".
+#   
+#   Enter the password to access level 2:
+    
+
 s = "Fourscoreandsevenyearsagoourfaathersbroughtforthonthiscontainentane"  \
 "wnationconceivedinzLibertyanddedicatedtothepropositionthatallmenarecrea"  \
 "tedequalNowweareengagedinagreahtcivilwartestingwhetherthatnaptionoranyn"  \
--- 3_subsum.py
+++ 3_subsum.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# The Greplin Programming Challenge
+# 	
+# 	Level 3
+# 	
+# 	----------------------------------------
+# 
+# 	Congratulations.  You have reached the final level.
+# 	
+# 	For the final task, you must find all subsets of an array
+# 	where the largest number is the sum of the remaining numbers.
+# 	For example, for an input of:
+# 
+# 	(1, 2, 3, 4, 6)
+# 
+# 	the subsets would be
+# 
+# 	1 + 2 = 3
+# 	1 + 3 = 4
+# 	2 + 4 = 6
+# 	1 + 2 + 3 = 6
+# 	
+# 	Here is the list of numbers you should run your code on.
+# 	The password is the number of subsets.  In the above case the
+# 	answer would be 4.
+# 	
+# 	Enter the password to complete the challenge: 
+
 import itertools
 
 # l = (1, 2, 3, 4, 6)
-l = (3, 4, 9, 14, 15, 19, 28, 37, 47, 50, 54, 56, 59, 61, 70, 73, 78, 81, 92, 95, 97, 99)
+l = (3, 4, 9, 14, 15, 19, 28, 37, 47, 50, 54, 56, 59, 61, 70, 
+	73, 78, 81, 92, 95, 97, 99)
 
 counter = 0
 
@@ -12,6 +40,6 @@ for r in range(2, len(l) + 1):
     for subset in itertools.combinations(l, r=r):
         if subset[-1] == sum(subset[:-1]):
             counter += 1
-            print subset
+            # print subset
 
 print "Password level 3:", counter
